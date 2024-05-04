403e02f2b8810014d70e0718b100c871e5aff72a
Evgeny Troshin <dylan_jt@mail.ru>
2024-05-05 01:11:39 +0300

    Задание 4 - Полевые испытания

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -66,46 +66,139 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-student_1 = Student('Ruoy', 'Eman', 'your_gender')
+def average_rating_courses(students, courses):
+    average_rating = {}
+    for course in courses:
+        all_rating_cource = []
+        for student in students:
+            all_rating_cource += student.grades[course]
+        average_rating[course] = round(sum(all_rating_cource) / len(all_rating_cource), 1)
+    return average_rating
+
+def average_rating_lecturers(lecturers, courses):
+    average_rating = {}
+    for course in courses:
+        all_rating_lecturer = []
+        for lecturer in lecturers:
+            all_rating_lecturer += lecturer.grades[course]
+        average_rating[course] = round(sum(all_rating_lecturer) / len(all_rating_lecturer), 1)
+    return average_rating
+
+student_1 = Student('Ruoy', 'Eman', 'Man')
 student_1.finished_courses += ['Введение в програмирование']
 student_1.courses_in_progress += ['Python']
 student_1.courses_in_progress += ['Git']
 
+student_2 = Student('Kevin', 'Feige', 'Man')
+student_2.finished_courses += ['Введение в програмирование']
+student_2.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Git']
+
 mentor1 = Lecturer('Питер', 'Паркер')
-mentor2 = Reviewer('Мери', 'Отсон')
 mentor1.courses_attached += ['Python']
+mentor1.courses_attached += ['Git']
+
+mentor2 = Lecturer('Гарри', 'Озборн')
 mentor2.courses_attached += ['Python']
 mentor2.courses_attached += ['Git']
 
-mentor2.rate_hw(student_1, 'Python', 10)
-mentor2.rate_hw(student_1, 'Git', 10)
-mentor2.rate_hw(student_1, 'Python', 10)
-mentor2.rate_hw(student_1, 'Git', 9)
-mentor2.rate_hw(student_1, 'Python', 10)
-mentor2.rate_hw(student_1, 'Git', 10)
-mentor2.rate_hw(student_1, 'Python', 10)
+mentor3 = Reviewer('Мери', 'Отсон')
+mentor3.courses_attached += ['Python']
+mentor3.courses_attached += ['Git']
 
+mentor4 = Reviewer('Фелиция', 'Харди')
+mentor4.courses_attached += ['Python']
+mentor4.courses_attached += ['Git']
+
+students = [student_1, student_2]
+lecturers = [mentor1, mentor2]
+courses = ['Python', 'Git']
+
+mentor3.rate_hw(student_1, 'Python', 10)
+mentor3.rate_hw(student_1, 'Git', 10)
+mentor3.rate_hw(student_1, 'Python', 10)
+mentor3.rate_hw(student_1, 'Git', 9)
+mentor3.rate_hw(student_1, 'Python', 10)
+mentor3.rate_hw(student_1, 'Git', 10)
+mentor3.rate_hw(student_1, 'Python', 10)
+
+mentor3.rate_hw(student_2, 'Python', 9)
+mentor3.rate_hw(student_2, 'Git', 9)
+mentor3.rate_hw(student_2, 'Python', 10)
+mentor3.rate_hw(student_2, 'Git', 7)
+mentor3.rate_hw(student_2, 'Python', 10)
+mentor3.rate_hw(student_2, 'Git', 8)
+mentor3.rate_hw(student_2, 'Python', 10)
+
+mentor4.rate_hw(student_1, 'Python', 10)
+mentor4.rate_hw(student_1, 'Git', 10)
+mentor4.rate_hw(student_1, 'Python', 10)
+mentor4.rate_hw(student_1, 'Git', 9)
+mentor4.rate_hw(student_1, 'Python', 10)
+mentor4.rate_hw(student_1, 'Git', 10)
+mentor4.rate_hw(student_1, 'Python', 10)
+
+mentor4.rate_hw(student_2, 'Python', 10)
+mentor4.rate_hw(student_2, 'Git', 8)
+mentor4.rate_hw(student_2, 'Python', 9)
+mentor4.rate_hw(student_2, 'Git', 8)
+mentor4.rate_hw(student_2, 'Python', 10)
+mentor4.rate_hw(student_2, 'Git', 9)
+mentor4.rate_hw(student_2, 'Python', 10)
+
+student_1.grade_lecturer(mentor1, 'Python', 9)
+student_1.grade_lecturer(mentor1, 'Git', 8)
 student_1.grade_lecturer(mentor1, 'Python', 10)
-student_1.grade_lecturer(mentor1, 'Git', 10)
-student_1.grade_lecturer(mentor1, 'Python', 10)
-student_1.grade_lecturer(mentor1, 'Git', 10)
+student_1.grade_lecturer(mentor1, 'Git', 7)
 student_1.grade_lecturer(mentor1, 'Python', 10)
 student_1.grade_lecturer(mentor1, 'Git', 9)
 student_1.grade_lecturer(mentor1, 'Python', 10)
 
+student_1.grade_lecturer(mentor2, 'Python', 10)
+student_1.grade_lecturer(mentor2, 'Git', 9)
+student_1.grade_lecturer(mentor2, 'Python', 10)
+student_1.grade_lecturer(mentor2, 'Git', 8)
+student_1.grade_lecturer(mentor2, 'Python', 9)
+student_1.grade_lecturer(mentor2, 'Git', 9)
+student_1.grade_lecturer(mentor2, 'Python', 10)
+
+student_2.grade_lecturer(mentor1, 'Python', 10)
+student_2.grade_lecturer(mentor1, 'Git', 7)
+student_2.grade_lecturer(mentor1, 'Python', 6)
+student_2.grade_lecturer(mentor1, 'Git', 7)
+student_2.grade_lecturer(mentor1, 'Python', 8)
+student_2.grade_lecturer(mentor1, 'Git', 10)
+student_2.grade_lecturer(mentor1, 'Python', 9)
+
+student_2.grade_lecturer(mentor2, 'Python', 9)
+student_2.grade_lecturer(mentor2, 'Git', 10)
+student_2.grade_lecturer(mentor2, 'Python', 7)
+student_2.grade_lecturer(mentor2, 'Git', 9)
+student_2.grade_lecturer(mentor2, 'Python', 8)
+student_2.grade_lecturer(mentor2, 'Git', 9)
+student_2.grade_lecturer(mentor2, 'Python', 8)
+
 mentor1.average_rating(mentor1)
+mentor2.average_rating(mentor2)
 student_1.average_rating(student_1)
+student_2.average_rating(student_2)
 
 print(mentor1)
 print(mentor2)
+print(mentor3)
+print(mentor4)
 print(student_1)
+print(student_2)
 
-if student_1.rating:  # > student_2.rating:
+if student_1.rating > student_2.rating:
     print('Лучший студент', student_1.name, student_1.surname)
 else:
-    print('Лучший студент')  # , student_2.name, student_2.surname)
+    print('Лучший студент', student_2.name, student_2.surname)
 
-if mentor1.rating:  # > student_2.rating:
+if mentor1.rating > student_2.rating:
     print('Лучший лектор', mentor1.name, mentor1.surname)
 else:
-    print('Лучший лектор')  # , mentor1.name, mentor1.surname)
+    print('Лучший лектор', mentor1.name, mentor1.surname)
+
+print('Средняя оценка всех студентов в рамках курсов: ', average_rating_courses(students, courses))
+print('Средняя оценка всех лекторов в рамка курсов', average_rating_lecturers(lecturers, courses))
