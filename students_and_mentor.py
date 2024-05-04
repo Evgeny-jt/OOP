b7d82f19c59b480daf1544e666621dc83d59b2ba
Evgeny Troshin <dylan_jt@mail.ru>
2024-05-04 22:14:15 +0300

    Задание 1 - Наследование

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,13 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,15 +22,32 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
+
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
- 
+
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
- 
+
 print(best_student.grades)
+
+leser1 = Lecturer('Питер', 'Паркер')
+leser2 = Lecturer('Мери', 'Отсон')
+
+print(leser1.name, leser1.surname)
+print(leser2.name, leser2.surname)
