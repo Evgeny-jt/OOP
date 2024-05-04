b942a648a33574360c6c4fe87fbb38c2b1a2d5e6
Evgeny Troshin <dylan_jt@mail.ru>
2024-05-04 22:42:15 +0300

    Задание 3 - Полиморфизм и магические методы

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,6 +7,10 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        return 'Имя: {}\nФамилия: {}\nСредняя оценка за домашнее задание: {}\nКурсы в процессе изучения: {}\nЗавершенные курсы: {}'.format(
+            self.name, self.surname, self.rating, self.courses_in_progress, self.finished_courses)
+
     def grade_lecturer(self, mentor, course, grade):
         if isinstance(mentor, Lecturer) and course in self.courses_in_progress:
             if course in mentor.grades:
@@ -15,6 +19,12 @@ class Student:
                 mentor.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def average_rating(self, student):
+        full_grades = []
+        for key in student.grades:
+            full_grades += student.grades[key]
+        self.rating = round(sum(full_grades) / len(full_grades), 1)
 
 
 class Mentor:
@@ -29,10 +39,22 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.rating}'
+
+    def average_rating(self, mentor):
+        full_grades = []
+        for key in mentor.grades:
+            full_grades += mentor.grades[key]
+        self.rating = round(sum(full_grades) / len(full_grades), 1)
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -44,24 +66,46 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+student_1 = Student('Ruoy', 'Eman', 'your_gender')
+student_1.finished_courses += ['Введение в програмирование']
+student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['Git']
 
 mentor1 = Lecturer('Питер', 'Паркер')
 mentor2 = Reviewer('Мери', 'Отсон')
 mentor1.courses_attached += ['Python']
 mentor2.courses_attached += ['Python']
+mentor2.courses_attached += ['Git']
 
-mentor2.rate_hw(best_student, 'Python', 10)
-mentor2.rate_hw(best_student, 'Python', 10)
-mentor2.rate_hw(best_student, 'Python', 10)
+mentor2.rate_hw(student_1, 'Python', 10)
+mentor2.rate_hw(student_1, 'Git', 10)
+mentor2.rate_hw(student_1, 'Python', 10)
+mentor2.rate_hw(student_1, 'Git', 9)
+mentor2.rate_hw(student_1, 'Python', 10)
+mentor2.rate_hw(student_1, 'Git', 10)
+mentor2.rate_hw(student_1, 'Python', 10)
 
-best_student.grade_lecturer(mentor1, 'C#', 7)
-best_student.grade_lecturer(mentor1, 'Python', 9)
-best_student.grade_lecturer(mentor1, 'Python', 8)
+student_1.grade_lecturer(mentor1, 'Python', 10)
+student_1.grade_lecturer(mentor1, 'Git', 10)
+student_1.grade_lecturer(mentor1, 'Python', 10)
+student_1.grade_lecturer(mentor1, 'Git', 10)
+student_1.grade_lecturer(mentor1, 'Python', 10)
+student_1.grade_lecturer(mentor1, 'Git', 9)
+student_1.grade_lecturer(mentor1, 'Python', 10)
 
-print(mentor1.name, mentor1.surname)
-print(mentor2.name, mentor2.surname)
+mentor1.average_rating(mentor1)
+student_1.average_rating(student_1)
 
-print(best_student.name, best_student.grades)
-print(mentor1.name, mentor1.grades)
+print(mentor1)
+print(mentor2)
+print(student_1)
+
+if student_1.rating:  # > student_2.rating:
+    print('Лучший студент', student_1.name, student_1.surname)
+else:
+    print('Лучший студент')  # , student_2.name, student_2.surname)
+
+if mentor1.rating:  # > student_2.rating:
+    print('Лучший лектор', mentor1.name, mentor1.surname)
+else:
+    print('Лучший лектор')  # , mentor1.name, mentor1.surname)
